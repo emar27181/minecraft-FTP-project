70db5d9115b930de7c79eb38ade4d5b5f2679d3f
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2025-01-29 18:34:09 +0900

    latest.logを読み込む機能の実装

--- src/utils/extract_check_in_out.py
+++ src/utils/extract_check_in_out.py
@@ -2,8 +2,9 @@ def extract_check_in_out():
   """
     latest.logから入退室に関する情報を抽出する関数
   """
-  
-  print("test")
+  with open("src/data/output/latest.log", 'r')as file:
+    data = file.read()
+    print(data)
   
 if __name__ == '__main__':
   extract_check_in_out()
