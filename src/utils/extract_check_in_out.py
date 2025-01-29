4ed890158077500dcd098b7b8b46e76bdd19aac4
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2025-01-29 18:57:06 +0900

    入退室のログの時刻や，プレイヤー名などのデータを変数に保存する機能の追加

--- src/utils/extract_check_in_out.py
+++ src/utils/extract_check_in_out.py
@@ -8,12 +8,19 @@ def extract_check_in_out():
   log_data = log_data.split('\n')
   
   for log in log_data:
+    # 接続・切断に関するログだった場合
     if "connect" in log :
-      if "disconnect" in log:
-        print("disconnect")
-        
-      else:
-        print("connect")
+      splited_log = log.split(" ")
+      date,time, connection, player_name, xuid = splited_log[0], splited_log[1], splited_log[4], splited_log[5], splited_log[7]
+      
+      player_name = player_name.strip(",")
+      
+      # print(f"{date} {time} {connection} {player_name}")
+      if (connection == "disconnected"):
+        print(f" {player_name} が退出しました．")
+      else :
+        print(f" {player_name} が入室しました．")
+      
   
 if __name__ == '__main__':
   extract_check_in_out()
