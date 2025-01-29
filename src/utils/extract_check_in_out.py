95f2c38d8f48ca0a2ce49106318d53a104841447
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2025-01-29 18:41:05 +0900

    latest.logの中で接続，切断に関する行のみを抽出する機能の実装

--- src/utils/extract_check_in_out.py
+++ src/utils/extract_check_in_out.py
@@ -3,8 +3,17 @@ def extract_check_in_out():
     latest.logから入退室に関する情報を抽出する関数
   """
   with open("src/data/output/latest.log", 'r')as file:
-    data = file.read()
-    print(data)
+    log_data = file.read()
+    
+  log_data = log_data.split('\n')
+  
+  for log in log_data:
+    if "connect" in log :
+      if "disconnect" in log:
+        print("disconnect")
+        
+      else:
+        print("connect")
   
 if __name__ == '__main__':
   extract_check_in_out()
