7d06af5ee7ffdb695aa9cb804aa02a034d858357
ema_ryu-home-desktop-pc <ee207008@meiji.ac.jp>
2025-02-03 01:09:37 +0900

    不要となった関数の削除

--- src/utils/extract_check_in_out.py
+++ src/utils/extract_check_in_out.py
@@ -1,30 +1,3 @@
-def extract_check_in_out():
-    """
-      latest.logから入退室に関する情報を抽出する関数
-    """
-    with open("src/data/output/latest.log", 'r')as file:
-        log_data = file.read()
-
-    log_data = log_data.split('\n')
-
-    for log in log_data:
-        # 接続・切断に関するログだった場合
-        if "connect" in log:
-            is_log_about_check_in_out(log)
-            """
-            splited_log = log.split(" ")
-            date, time, connection, player_name, xuid = splited_log[0], splited_log[1], splited_log[4], splited_log[5], splited_log[7]
-
-            player_name = player_name.strip(",")
-
-            # print(f"{date} {time} {connection} {player_name}")
-            if (connection == "disconnected"):
-                print(f" {player_name} が退出しました．")
-            else:
-                print(f" {player_name} が入室しました．")
-            """
-
-
 def return_about_check_in_out(log_line):
     """
       引数で受け取ったログに入退室に関する表示文を返す関数
