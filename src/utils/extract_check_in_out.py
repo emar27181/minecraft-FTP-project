3ee9d836f79b34982993c0e2aaf75d94cece357d
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2025-02-16 01:19:04 +0900

    更新されたlatestの追加箇所を抽出する関数とlatest.logの末尾の行を更新する関数実装

--- src/utils/extract_check_in_out.py
+++ src/utils/extract_check_in_out.py
@@ -21,7 +21,8 @@ def return_about_check_in_out_be_edition(log_line):
     else:
         print(f" {player_name} が入室しました．")
         return f" {player_name} が入室しました．"
-    
+
+
 def return_about_check_in_out_java_edition(log_line):
     """
       引数で受け取ったログに入退室に関する表示文を返す関数(Java版用)
@@ -34,7 +35,7 @@ def return_about_check_in_out_java_edition(log_line):
     """
 
     splited_log = log_line.split(" ")
-    time,  player_name,connection= splited_log[0], splited_log[3], splited_log[4]
+    time, player_name, connection = splited_log[0], splited_log[3], splited_log[4]
 
     if (connection == "left"):
         print(f" {player_name} が退出しました．")
@@ -42,6 +43,55 @@ def return_about_check_in_out_java_edition(log_line):
     else:
         print(f" {player_name} が入室しました．")
         return f" {player_name} が入室しました．"
+
+
+def update_latest_added_lines_log():
+    """更新されたlatest.logの追記個所を抽出する関数(抽出される行数>=1)
+    """
+
+    with open("src/data/output/latest.log", 'r')as file:
+        latest_log_data = file.read()
+        latest_log_data = latest_log_data.split('\n')
+        now_end_of_line_log_data = latest_log_data[len(latest_log_data) - 2]  # ログの末尾は空行なので，-2で最終行末尾を取得
+
+    with open("src/data/output/latest_end_of_line.log", 'r')as file:
+        latest_end_of_line_log_data = file.read()
+
+    print(f"latest_end_of_line_log_data = {latest_end_of_line_log_data}")
+    added_lines = []
+    for latest_log_one_line in reversed(latest_log_data):
+        print(f"latest_log_one_line = {latest_log_one_line}")
+
+        if (latest_log_one_line != latest_end_of_line_log_data):
+            added_lines.append(latest_log_one_line)
+        else:
+            break
+
+    added_lines.reverse()
+
+    print(f"new_log = {added_lines}")
+
+    output_file_path = "src/data/output/latest_added_lines.log"
+    with open(output_file_path, "w", encoding="utf-8") as f:
+        f.write("\n".join(added_lines))
+    print(f"{output_file_path} を更新しました．")
+
+
+def update_latest_end_of_line_log():
+    """latest.logの末尾の行を更新する関数
+    """
+
+    with open("src/data/output/latest.log", 'r')as file:
+        latest_log_data = file.read()
+        latest_log_data = latest_log_data.split('\n')
+        now_end_of_line_log_data = latest_log_data[len(latest_log_data) - 2]  # ログの末尾は空行なので，-2で最終行末尾を取得
+
+    output_file_path = "src/data/output/latest_end_of_line.log"
+
+    with open(output_file_path, "w", encoding="utf-8") as f:
+        f.write(now_end_of_line_log_data)
+
+    print(f"{output_file_path} を更新しました．")
 
 
 def is_log_updated():
@@ -72,8 +122,6 @@ def is_log_updated():
     # 現在の末尾のログのが前回の末尾のログと異なる場合
     else:
         print(f"=== 新しいログがあります．=============\n{latest_end_of_line_log_data} -> {now_end_of_line_log_data}")
-        with open("src/data/output/latest_end_of_line.log", "w", encoding="utf-8") as f:
-            f.write(now_end_of_line_log_data)
         return True
 
     # print(latest_log_data)
@@ -95,6 +143,7 @@ def is_latest_end_of_line_log_about_check_in_out_be_edition():
 
     return "Player" in latest_end_of_line_log_data
 
+
 def is_latest_end_of_line_log_about_check_in_out_java_edition():
     """
         latest_end_of_line.logの最終行が入退室に関するログか確認する関数(Java用)
@@ -110,4 +159,3 @@ def is_latest_end_of_line_log_about_check_in_out_java_edition():
         latest_end_of_line_log_data = file.read()
 
     return "the game" in latest_end_of_line_log_data
-
--- src/webhook.py
+++ src/webhook.py
@@ -1,7 +1,7 @@
 import os
 import requests
 from dotenv import load_dotenv
-from utils.extract_check_in_out import is_latest_end_of_line_log_about_check_in_out_java_edition, is_log_updated, return_about_check_in_out_java_edition
+from utils.extract_check_in_out import is_latest_end_of_line_log_about_check_in_out_java_edition, is_log_updated, return_about_check_in_out_java_edition, update_latest_added_lines_log, update_latest_end_of_line_log
 
 # .env を読み込む
 load_dotenv()
@@ -28,8 +28,12 @@ def is_send_discord_message_about_check_in_out():
     Discordにメッセージを送信するか確認する関数
     """
     if is_log_updated():
-        if is_latest_end_of_line_log_about_check_in_out_java_edition():
-            return True
+
+        update_latest_added_lines_log()  # latest.logに追加された行を更新
+        update_latest_end_of_line_log()  # latest
+
+        # if is_latest_end_of_line_log_about_check_in_out_java_edition():
+        return True
     else:
         return False
 
@@ -41,5 +45,5 @@ if __name__ == "__main__":
     if (is_send_discord_message_about_check_in_out()):
         with open("src/data/output/latest_end_of_line.log", 'r')as file:
             latest_end_of_line_log_data = file.read()
-        send_discord_message(return_about_check_in_out_java_edition(latest_end_of_line_log_data))
+        # send_discord_message(return_about_check_in_out_java_edition(latest_end_of_line_log_data))
     # send_discord_message("HelloWorld")
