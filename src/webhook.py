f3063f373df2ef1d33705fa389e1a7c9aa0c0f42
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2025-02-16 01:22:34 +0900

    取得したlatest.logから変更箇所を更新(抽出)する関数の実装

--- src/webhook.py
+++ src/webhook.py
@@ -23,27 +23,26 @@ def send_discord_message(message):
         print(f"❌ 送信エラー: {response.status_code}, {response.text}")
 
 
-def is_send_discord_message_about_check_in_out():
+def update_latest_changed_log():
     """
-    Discordにメッセージを送信するか確認する関数
+    取得したlatest.logから変更箇所を更新(抽出)する関数
     """
     if is_log_updated():
 
-        update_latest_added_lines_log()  # latest.logに追加された行を更新
-        update_latest_end_of_line_log()  # latest
+        update_latest_added_lines_log()  # latest.logに追加された行の更新
+        update_latest_end_of_line_log()  # latest.logの末尾の行の更新
 
-        # if is_latest_end_of_line_log_about_check_in_out_java_edition():
-        return True
-    else:
-        return False
 
 
 if __name__ == "__main__":
 
     # print(is_send_discord_message_about_check_in_out())
 
-    if (is_send_discord_message_about_check_in_out()):
+    update_latest_changed_log()
+    """
+    if (send_discord_message_about_check_in_out()):
         with open("src/data/output/latest_end_of_line.log", 'r')as file:
             latest_end_of_line_log_data = file.read()
         # send_discord_message(return_about_check_in_out_java_edition(latest_end_of_line_log_data))
     # send_discord_message("HelloWorld")
+    """
